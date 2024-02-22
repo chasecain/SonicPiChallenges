f1253d889f0a5fed5665f4c211fa5fdd0c6b8aa6
chasecain <160057772+chasecain@users.noreply.github.com>
2024-02-22 10:27:44 -0800

    Update and rename .rb to challengeD.rb

--- challengeD.rb
+++ challengeD.rb
@@ -1,5 +1,46 @@
+# CHALLENGE D: Use functions to optimize the SpongeBob theme!
+
 use_bpm 136
 use_synth :piano
+
+define :measure2and8 do
+  sample :bass_hit_c
+  play:E4
+  sleep 1
+  sample :bass_hit_c
+  play:E5
+  sample :bass_hit_c
+  play:Gs4
+  sleep 2
+  sample :bass_hit_c
+  play:E5
+  sample :bass_hit_c
+  play:Gs4
+  sleep 1
+  sample :hat_psych
+end
+
+define :measure3and4and6 do
+  sample :bass_hit_c
+  play :r
+  sleep 1
+  sample :bass_hit_c
+  play:E5
+  sample :bass_hit_c
+  play:Gs4
+  sample :bass_hit_c
+  play:B4
+  sleep 1
+  sample :bass_hit_c
+  play:B4
+  sleep 1
+  sample :bass_hit_c
+  play:E5
+  sample :bass_hit_c
+  play:Gs4
+  sleep 1
+  sample :hat_psych
+end
 
 
 live_loop:background_notes do
@@ -71,6 +112,14 @@ live_loop:background_notes do
   stop
 end
 
+live_loop:melody do
+  16.times do
+    sleep 2
+    sample :drum_cowbell
+  end
+  stop
+end
+
 # Measure 1
 sample :bass_hit_c
 play :r
@@ -88,62 +137,13 @@ sleep 1
 sample :hat_psych
 
 # Measure 2
-sample :bass_hit_c
-play:E4
-sleep 1
-sample :bass_hit_c
-play:E5
-sample :bass_hit_c
-play:Gs4
-sleep 2
-sample :bass_hit_c
-play:E5
-sample :bass_hit_c
-play:Gs4
-sleep 1
-sample :hat_psych
+measure2and8
 
 # Measure 3
-sample :bass_hit_c
-play :r
-sleep 1
-sample :bass_hit_c
-play:E5
-sample :bass_hit_c
-play:Gs4
-sample :bass_hit_c
-play:B4
-sleep 1
-sample :bass_hit_c
-play:B4
-sleep 1
-sample :bass_hit_c
-play:E5
-sample :bass_hit_c
-play:Gs4
-sleep 1
-sample :hat_psych
+measure3and4and6
 
 # Measure 4
-sample :bass_hit_c
-play :r
-sleep 1
-sample :bass_hit_c
-play:E5
-sample :bass_hit_c
-play:Gs4
-sample :bass_hit_c
-play:B4
-sleep 1
-sample :bass_hit_c
-play:B4
-sleep 1
-sample :bass_hit_c
-play:E5
-sample :bass_hit_c
-play:Gs4
-sleep 1
-sample :hat_psych
+measure3and4and6
 
 # Measure 5
 sample :bass_hit_c
@@ -171,25 +171,7 @@ sleep 1
 sample :hat_psych
 
 # Measure 6
-sample :bass_hit_c
-play :r
-sleep 1
-sample :bass_hit_c
-play:E5
-sample :bass_hit_c
-play:Gs4
-sample :bass_hit_c
-play:B4
-sleep 1
-sample :bass_hit_c
-play:B4
-sleep 1
-sample :bass_hit_c
-play:E5
-sample :bass_hit_c
-play:Gs4
-sleep 1
-sample :hat_psych
+measure3and4and6
 
 # Measure 7
 sample :bass_hit_c
@@ -216,20 +198,9 @@ sleep 1
 sample :hat_psych
 
 # Measure 8
-sample :bass_hit_c
-play:E4
-sleep 1
-sample :bass_hit_c
-play:E5
-sample :bass_hit_c
-play:Gs4
-sleep 2
-sample :bass_hit_c
-play:E5
-sample :bass_hit_c
-play:Gs4
-sleep 1
-sample :hat_psych
+measure2and8
+
+
 
 sample :bass_hard_c
 sleep 1
